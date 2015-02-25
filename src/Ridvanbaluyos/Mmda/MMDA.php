f4efa67b8e2ed628071e7d61dbaba2e8ab19bc4b
Arnel Labarda <aqlx86@gmail.com>
2015-02-26 06:24:27 +0800

    minor refactor

--- src/Ridvanbaluyos/Mmda/MMDA.php
+++ src/Ridvanbaluyos/Mmda/MMDA.php
@@ -2,40 +2,40 @@
 
 class MMDA
 {
+    CONST FEED_URL = 'http://mmdatraffic.interaksyon.com/livefeed/';
+
     private $trafficData;
     private $channel;
 
-    public function __construct($mustInit = false)
+    public function __construct()
     {
         $this->trafficData = $this->getTrafficData();
     }
 
-    public function traffic()
+    public function traffic($highway, $segment = NULL)
     {
-        if (isset($this->trafficData)) {
-            return $this->trafficData;
-        } else {
-            return null;
-        }
+        $traffic = $this->trafficData[$highway];
+
+        if ($segment)
+            $traffic = $traffic[$segment];
+
+        return $traffic;
     }
 
-    public function highways()
+    public function get_highways()
     {
-        if (isset($this->trafficData)) {
+        if ($this->trafficData)
             return array_keys($this->trafficData);
-        } else {
-            return null;
-        }
+
+        return null;
     }
 
-    public function segments($highway = '')
+    public function get_highway_segments($highway = NULL)
     {
-        if (isset($this->trafficData[$highway])) {
+        if ($highway && isset($this->trafficData[$highway]))
             return array_keys($this->trafficData[$highway]);
-        } else {
-            return null;
-        }
 
+        return null;
     }
 
     final private function getTrafficData()
@@ -43,16 +43,14 @@ class MMDA
         $ch = curl_init();
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'http://mmdatraffic.interaksyon.com/livefeed/');
+        curl_setopt($ch, CURLOPT_URL, self::FEED_URL);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $response = curl_exec($ch);
         curl_close($ch);
 
         $xml = simplexml_load_string($response);
 
-        $array = $this->parseTrafficData($xml);
-
-        return $array;
+        return $this->parseTrafficData($xml);
     }
 
     final private function parseTrafficData($xml)
